d6af53a1d60b15c04b8361ef9c81de2e993eb86f
Ibramsou <kasobanboui@gmail.com>
2023-07-17 10:58:33 +0200

    initial commit

---
+++ src/main/java/fr/bramsou/protocol/ProtocolChecker.java
@@ -0,0 +1,14 @@
+package fr.bramsou.protocol;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ProtocolChecker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
